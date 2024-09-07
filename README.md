cd2cc1ae05f945f74e740dbe630129a5d34847d1
D Rupesh Kumar <drupesh2008@gmail.com>
2024-09-07 13:52:24 +0530

    initial version

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
-# spreadsheet-using-rpn
-Evaluate Cell Values in Spreadsheet comprising of Formulaes using RPN and Stack
+# SpreadSheet Calculator using RPN
+
+This python program is an abstract implementation of how values in Cells in Spreadsheet are computed using the concept of RPN and the use of stack to evaluate those values for the formulaes in each given cell.
+
+The input is to read a spreadsheet from `stdin`, evaluate the values of all the cells and write the output to `stdout`.
+
+The spreadsheet input is defined as follows:
+<br/>● Line 1: two integers, defining the width and height of the spreadsheet (n, m)
+<br/>● n*m lines each containing an expression which is the value of the corresponding cell
+(cells enumerated in the order A1, A2, A<n>, B1, …)
+
+Visual Representation of Input and Output is as follows:
+![Input-Output example](input-output.png)
+
+### Algorithm:
+1. Read all the input from `stdin` line by line
+2. Store each cell expression to a global dictionary
+3. Iterate through all cell expression - add it to set of cells that needs to be evaluated if that cell value is not already computed.
+4. For each expression in evaluated set - use the RPN function to parse and analyze the expression, the result of which can be stored in the corresponding computed cell dictionary.
+5. If a cell reference already exists in evaluated cells, then a cycle has been detected, post which it exits with non-zero code.
+6. Store and format the result with double precision and output the final values.
+
+### Input Test Cases:
+1. input.txt -> contains the input as mentioned in the description above.
+2. unary.txt -> contains the input which has unary operator
+3. cycle.txt -> contains the input which throws Cycle Dependency Error along with cell reference number
+
+### To run the code:
+```
+cat input.txt | python main.py
+cat unary.txt | python main.py
+cat cycle.txt | python main.py
+```
---
+++ main.py
@@ -0,0 +1,111 @@
+import sys
+
+class CycleDetectedError(Exception):
+    pass
+
+class SpreadSheetCalc:
+    def __init__(self):
+        # dictionary to map cell names to their raw expressions
+        self.cell_expressions = {}
+
+    # Process Reverse Polish Notation (RPN) expression
+    def process_rpn_expression(self, expression_parts, computed_values, visited_cells):
+        stack = []
+        for part in expression_parts:
+            # Numbers and negative numbers
+            if part.isdigit() or (part[0] == '-' and part[1:].isdigit()):
+                stack.append(float(part))
+            elif part in computed_values:  # Already computed cell
+                stack.append(computed_values[part])
+            elif part[0].isalpha() and part[1:].isdigit():  # Cell reference for A1, A2, .... B1, B2...
+                if part in visited_cells:  # Check for cycles
+                    raise CycleDetectedError(f"Cycle detected at cell {part}")
+                stack.append(self.evaluate_single_cell(part, computed_values, visited_cells))
+            elif part in ['+', '-', '*', '/']:  # Binary Operators so pop two operands
+                b = stack.pop()
+                a = stack.pop()
+                if part == '+':
+                    stack.append(a + b)
+                elif part == '-':
+                    stack.append(a - b)
+                elif part == '*':
+                    stack.append(a * b)
+                elif part == '/':
+                    stack.append(a / b)
+            elif part == '++':  # Increment operator
+                value = stack.pop()
+                stack.append(value + 1)
+            elif part == '--':  # Decrement operator
+                value = stack.pop()
+                stack.append(value - 1)
+            else:
+                raise ValueError(f"Unknown operator or value: {part}")
+        
+        return stack.pop()
+
+    # Function to evaluate a single cell
+    def evaluate_single_cell(self, cell_name, computed_values, visited_cells):
+        visited_cells.add(cell_name)  # Mark this cell as being evaluated
+        expression = self.cell_expressions[cell_name].split()
+        result = self.process_rpn_expression(expression, computed_values, visited_cells)
+        computed_values[cell_name] = result
+        visited_cells.remove(cell_name)  # Done evaluating
+        return result
+
+    # Function to compute all cell values, with cycle detection
+    def compute_all_cell_values(self, width, height):
+        computed_values = {}
+        # To track cells being evaluated/visited
+        visited_cells = set()
+
+        for row in range(height):
+            for col in range(width):
+                # A1, B2, etc...
+                cell_name = f'{chr(65 + row)}{col + 1}'
+                if cell_name not in computed_values:
+                    try:
+                        self.evaluate_single_cell(cell_name, computed_values, visited_cells)
+                    except CycleDetectedError as e:
+                        print(f"Error: {str(e)}")
+                        # Non-zero exit code for cyclic dependency
+                        sys.exit(1)
+        
+        return computed_values
+
+def main():
+    input_data = sys.stdin.read().splitlines()
+
+    # Read the first line to get width and height
+    width_height = input_data[0].split()
+    no_of_cols = int(width_height[0])
+    no_of_rows = int(width_height[1])
+
+    # Sanity checks for input values of number of rows and cols
+    if no_of_cols < 0 or no_of_rows < 0:
+        print(f"Error: Number of rows or columns cannot be negative.")
+        sys.exit(2)
+
+    # Create the SpreadSheet Calculator Object
+    ss_calc_obj = SpreadSheetCalc()
+
+    # Read each cells expression
+    input_idx = 1
+    for row in range(no_of_rows):
+        for col in range(no_of_cols):
+            # A1, B2, etc.. [row computed using ASCII computation]
+            cell_name = f'{chr(65 + row)}{col + 1}' 
+            ss_calc_obj.cell_expressions[cell_name] = input_data[input_idx]
+            input_idx += 1
+
+    # Compute all the cell values
+    evaluated_values = ss_calc_obj.compute_all_cell_values(no_of_cols, no_of_rows)
+
+    # Output the result in the required format
+    print(f"{no_of_cols} {no_of_rows}")
+    for row in range(no_of_rows):
+        for col in range(no_of_cols):
+            cell_name = f'{chr(65 + row)}{col + 1}'
+            print(f"{evaluated_values[cell_name]:.5f}")
+
+if __name__ == "__main__":
+    main()
